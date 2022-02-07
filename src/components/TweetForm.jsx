0ce14ce21819b2926edb4dd1e0f5c12be1738c45
wachiye wanyama <wachiye@wachiyes-MacBook-Pro.local>
2022-02-07 13:15:55 +0300

    update form

--- src/components/TweetForm.jsx
+++ src/components/TweetForm.jsx
@@ -22,22 +22,23 @@ import {Context}  from '../context/Store';
 
     //rerender when state changes
     useEffect(()=>{
-    },[state.tweets, state.tweet])
+        console.log(inputs)
+    },[state.tweet])
 
      return(
         <form onSubmit={handleSubmit}>
             <p className="label">Create Post</p>
-            <textarea className="textarea" name="tweetText" onChange={handleInputChange}  value={inputs.tweetText}></textarea>
+            <textarea className="textarea" name="tweetText" onChange={handleInputChange}  defaultValue={state.tweet?.tweetText}></textarea>
             {errors.tweetText && <p className="error-message">{errors.tweetText}</p>}
             
             <p className="label">Scheduled Date and Time</p>
             <div className="flex-row-btwn">
                 <div className="textfield-wrapper">
-                    <input className="textField" type="date"  name="tweetDate" onChange={handleInputChange} value={inputs.tweetDate}></input>
+                    <input className="textField" type="date"  name="tweetDate" onChange={handleInputChange} defaultValue={state.tweet?.tweetDate}></input>
                     {errors.tweetDate && <p className="error-message">{errors.tweetDate}</p>}
                 </div>
                 <div className="textfield-wrapper">
-                    <input className="textField" type="time" name="tweetTime" onChange={handleInputChange} value={inputs.tweetTime}></input>
+                    <input className="textField" type="time" name="tweetTime" onChange={handleInputChange} defaultValue={state.tweet?.tweetTime}></input>
                     {errors.tweetTime && <p className="error-message">{errors.tweetTime}</p>}
                 </div>
             </div>
